3eef9596ea41b850b42e5cf581774135d8267ec7
xzartsust <68950796+xzartsust@users.noreply.github.com>
2020-07-31 09:55:54 +0300

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -217,18 +217,22 @@ async def help(ctx):
 
     emb= discord.Embed(title='Помощ по использованию бота', description='Здесь вы можете узнать про осноные команды бота')
 
+                       
+                       
     emb1= discord.Embed(title='Команды бота')
     emb1.add_field(name='`{}user @имя`'.format(PREFIX),value=' - Информация про пользователя', inline=False)
     emb1.add_field(name='`{}help` или `{}info` или `{}i`'.format(PREFIX,PREFIX,PREFIX),value=' - Команды бота', inline=False)
     emb1.add_field(name='`{}ping`'.format(PREFIX),value=' - Посмотреть пинг бота', inline=False)
     emb1.add_field(name='`{}bot_servers`'.format(PREFIX),value=' - Посмотреть на скольких серверах есть етот бот', inline=False)
-
+    emb1.add_field(name='`{}tuser`'.format(PREFIX), value=' - Посмотреть сколько всего человек используют этого бота',inline=False)
+    
     emb2=discord.Embed(title='Команды для модерации')
     emb2.add_field(name='`{}ban @имя причина`'.format(PREFIX),value=' - Выдать бан игрок', inline=False)
     emb2.add_field(name='`{}mute @имя`'.format(PREFIX),value=' - Замутить игрока', inline=False)
     emb2.add_field(name='`{}unban @имя`'.format(PREFIX),value='- Розбанить играка на етом сервере', inline=False)
     emb2.add_field(name='`{}kick @имя причина`'.format(PREFIX),value='- Вигнать игрока с етого сервера', inline=False)
     emb2.add_field(name='`{}clear`'.format(PREFIX),value='- Очыстка чата', inline=False)
+    
 
     embeds=[emb,emb1,emb2]
     message= await ctx.send(embed= emb1)
