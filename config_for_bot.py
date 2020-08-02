f52b5a7486d2effdcd155207944276e0260b9dc9
xzartsust <igru1234567@gmail.com>
2020-08-02 08:52:31 +0300

    update

--- config_for_bot.py
+++ config_for_bot.py
@@ -1,12 +1,5 @@
 PREFIX='.'
 
-# list for привествий
-greetings=['hello', 'hi', 'привіт','привет']
-goodbye=['bb','bye','пока','до скорого']
-help_for_server=['help','помощ','допомога','команди сервера']
-exc=['.джоін','.леаве','.плей', '.юзер','.хелп','.клеар']
-
-
 ydl_options={
     'format' : 'bestaudio/best',
     'postprocessors' : [{
