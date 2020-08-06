34c1d506ffce01c10741e0dff6553ea70c8948b8
xzartsust <igru1234567@gmail.com>
2020-08-06 21:01:32 +0300

    update

--- cogs/commands/prefix.py
+++ cogs/commands/prefix.py
@@ -25,6 +25,7 @@ class prefix(commands.Cog):
     async def on_guild_remove(self):
         pass
 
+
     @commands.command()
     @commands.has_permissions(administrator=True)
     async def prefix(self, ctx, prefix):
@@ -38,4 +39,4 @@ class prefix(commands.Cog):
 def setup(bot):
     bot.add_cog(prefix(bot))
 
-url = os.environ.get('url_bd')
+url = os.environ.get('DATABASE_URL')
