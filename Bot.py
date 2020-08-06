d826d553a85bc662f7bd39a97adb6233bf8dccc2
xzartsust <igru1234567@gmail.com>
2020-08-06 10:42:24 +0300

    update

--- Bot.py
+++ Bot.py
@@ -88,6 +88,7 @@ async def tuser(ctx):
 
 @bot.command()
 async def news(ctx,channel:discord.TextChannel, *,text):
+    await ctx.channel.purge(limit=1)
     emb= discord.Embed(title='Новость!!!',description=f'{text}', colour= discord.Color.teal(),timestamp=ctx.message.created_at)
     emb.set_footer(text=f'{ctx.message.author}' + ' создал эту новость!')
     await channel.send(embed=emb)
