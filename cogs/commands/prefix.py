27b34ed2d444d48d823f26f06b0b733e01250f3c
xzartsust <igru1234567@gmail.com>
2020-08-10 15:27:08 +0300

    update

--- cogs/commands/prefix.py
+++ cogs/commands/prefix.py
@@ -38,7 +38,7 @@ class prefix(commands.Cog):
 
     @commands.command()
     @commands.has_permissions(administrator=True)
-    async def prefix(self, ctx, prefix):
+    async def prefix(self, ctx):
         cursor.execute('INSERT INTO public."prefixDB" (guild_id, prefix) VALUES (1,2)')
 
 
