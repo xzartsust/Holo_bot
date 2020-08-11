d4e343861a593efd18f94212c32b2a3c22dbad1e
xzartsust <68950796+xzartsust@users.noreply.github.com>
2020-08-11 19:35:54 +0300

    Updated

--- cogs/commands/prefix.py
+++ cogs/commands/prefix.py
@@ -45,7 +45,8 @@ class prefix(commands.Cog):
         guildid = ctx.guild.id
         cursor.execute(f'UPDATE public."prefixDB" SET prefix=\'{prefix}\' WHERE guild_id = \'{guildid}\';')
         conn.commit()
-
+        emb = discord.Embed(title='Выполнено успешно!', description=f'Префикс сервера изменений на {prefix}',timestamp= ctx.message.created_at)
+        ctx.send(embed= emb)
 
 def setup(bot):
     bot.add_cog(prefix(bot))
