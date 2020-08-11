1aa0a8b7e2b8a55ab7236e1ff836387f2818a970
xzartsust <igru1234567@gmail.com>
2020-08-11 22:15:56 +0300

    update

--- cogs/commands/HelpCommands.py
+++ cogs/commands/HelpCommands.py
@@ -31,8 +31,6 @@ def prefix_in_guild(bot,message):
     
     return prefix
 
-PREFIX = prefix_in_guild
-
 
 class HelpCommands(commands.Cog):
     def __init__(self, bot):
@@ -43,12 +41,12 @@ class HelpCommands(commands.Cog):
         await ctx.channel.purge(limit=1)
 
         emb= discord.Embed(title=f'Команды бота {self.bot.user.name}', description='Здесь вы узнаете информацию про все команды бота\n')
-        emb.add_field(name='**Другая информация**',value='Чтобы получить больше информации о какой либо команде, вы можете написать: {}help `команда` \nТак же, вы можете нажать на реакцию под сообщением, чтобы переключить страницу.\n'.format(PREFIX))
+        emb.add_field(name='**Другая информация**',value='Чтобы получить больше информации о какой либо команде, вы можете написать: {}help `команда` \nТак же, вы можете нажать на реакцию под сообщением, чтобы переключить страницу.\n'.format(prefix_in_guild))
         
-        emb1= discord.Embed(title='Команды информации', description='Что бы узнать больше о команде напишите {}help [команда]. \n**Пример**: {}help user'.format(PREFIX,PREFIX))
-        emb1.add_field(name='**Команды**', value=f'`{PREFIX}user`\n`{PREFIX}ping`\n`{PREFIX}bot_servers`\n`{PREFIX}tuser`\n')
+        emb1= discord.Embed(title='Команды информации', description='Что бы узнать больше о команде напишите {}help [команда]. \n**Пример**: {}help user'.format(prefix_in_guild,prefix_in_guild))
+        emb1.add_field(name='**Команды**', value=f'`{prefix_in_guild}user`\n`{prefix_in_guild}ping`\n`{prefix_in_guild}bot_servers`\n`{prefix_in_guild}tuser`\n')
         
-        emb2=discord.Embed(title='Команды администрации', description=f'`{PREFIX}prefix`')
+        emb2=discord.Embed(title='Команды администрации', description=f'`{prefix_in_guild}prefix`')
 
         embeds=[emb,emb1,emb2]
         message= await ctx.send(embed= emb)
@@ -58,22 +56,22 @@ class HelpCommands(commands.Cog):
     
     @help_for_commands.command(name='user')
     async def user_subcommands(self, ctx):
-        user_emb=discord.Embed(title=f'Информация про команду: {PREFIX}user', colour = discord.Color.teal(), description=f'**Команда**: `[user]` или `[userinfo]` или `[infouser]` или `[iu]` или `[ui]`\n**Описание**: показивает информацию про пользователя\n**Использования**: `{PREFIX}user` или `{PREFIX}userinfo` или `{PREFIX}infouser` или `{PREFIX}iu` или `{PREFIX}ui`, или если вы хотите узнать информацию о другом пользователя, то после команды пропишите тег пользователя о котором хотите узнать информацию\n**Пример**: `{PREFIX}user @имя_пользователя`')
+        user_emb=discord.Embed(title=f'Информация про команду: {prefix_in_guild}user', colour = discord.Color.teal(), description=f'**Команда**: `[user]` или `[userinfo]` или `[infouser]` или `[iu]` или `[ui]`\n**Описание**: показивает информацию про пользователя\n**Использования**: `{prefix_in_guild}user` или `{prefix_in_guild}userinfo` или `{prefix_in_guild}infouser` или `{prefix_in_guild}iu` или `{prefix_in_guild}ui`, или если вы хотите узнать информацию о другом пользователя, то после команды пропишите тег пользователя о котором хотите узнать информацию\n**Пример**: `{prefix_in_guild}user @имя_пользователя`')
         await ctx.send(embed=user_emb)
 
     @help_for_commands.command(name='ping')
     async def pind_subcommands(self,ctx):
-        ping_emb=discord.Embed(title=f'Информация про команду: {PREFIX}ping',colour = discord.Color.teal(), description=f'**Команда**: `[ping]`\n**Описание**: показивает пинг бота\n**Использования**: `{PREFIX}ping`')
+        ping_emb=discord.Embed(title=f'Информация про команду: {prefix_in_guild}ping',colour = discord.Color.teal(), description=f'**Команда**: `[ping]`\n**Описание**: показивает пинг бота\n**Использования**: `{prefix_in_guild}ping`')
         await ctx.send(embed=ping_emb)
 
     @help_for_commands.command(name='bot_servers')
     async def botservers_subcommands(self,ctx):
-        botservers_emb=discord.Embed(title=f'Информация про команду: {PREFIX}bot_servers',colour = discord.Color.teal(), description=f'**Команда**: `[bot_servers]`\n**Описание**: показивает на сколько серверах присутствует этот бот\n**Использования**: `{PREFIX}bot_servers`')
+        botservers_emb=discord.Embed(title=f'Информация про команду: {prefix_in_guild}bot_servers',colour = discord.Color.teal(), description=f'**Команда**: `[bot_servers]`\n**Описание**: показивает на сколько серверах присутствует этот бот\n**Использования**: `{prefix_in_guild}bot_servers`')
         await ctx.send(embed=botservers_emb)   
 
     @help_for_commands.command(name='tuser')
     async def tuser_subcommands(self,ctx):
-        tuser_emb=discord.Embed(title=f'Информация про команду: {PREFIX}tuser',colour = discord.Color.teal(), description=f'**Команда**: `[tuser]`\n**Описание**: показивает сколько людей используют этого бота\n**Использования**: `{PREFIX}tuser`')
+        tuser_emb=discord.Embed(title=f'Информация про команду: {prefix_in_guild}tuser',colour = discord.Color.teal(), description=f'**Команда**: `[tuser]`\n**Описание**: показивает сколько людей используют этого бота\n**Использования**: `{prefix_in_guild}tuser`')
         await ctx.send(embed=tuser_emb)   
 
 
