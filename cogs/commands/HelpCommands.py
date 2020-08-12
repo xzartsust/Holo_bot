f92d2ea8fc7bba33467cd244c48f0d214405febd
xzartsust <igru1234567@gmail.com>
2020-08-12 15:53:00 +0300

    update

--- cogs/commands/HelpCommands.py
+++ cogs/commands/HelpCommands.py
@@ -22,15 +22,13 @@ conn = psycopg2.connect(
     port = "5432"
 )
 cursor = conn.cursor()
-'''
-def prefix_in_guild(bot,message):
+
+def get_prefix(bot, message):
     guildid = message.guild.id
     cursor.execute(f'SELECT prefix FROM public."prefixDB" WHERE guild_id = \'{guildid}\';')
     prefix = cursor.fetchone()
     conn.commit()
-    
     return prefix
-'''
 
 class HelpCommands(commands.Cog):
     def __init__(self, bot):
@@ -38,21 +36,17 @@ class HelpCommands(commands.Cog):
 
 
     @commands.group(name='help',aliases=['helpcmd','i','helpcommands'], invoke_without_command=True)
-    async def help_for_commands(self, ctx, message):
+    async def help_for_commands(self, ctx):
         await ctx.channel.purge(limit=1)
-        
-        guildid = message.guild.id
-        cursor.execute(f'SELECT prefix FROM public."prefixDB" WHERE guild_id = \'{guildid}\';')
-        prefix_in_guild = cursor.fetchone()
-        conn.commit()
+    
 
         emb= discord.Embed(title=f'Команды бота {self.bot.user.name}', description='Здесь вы узнаете информацию про все команды бота\n')
-        emb.add_field(name='**Другая информация**',value='Чтобы получить больше информации о какой либо команде, вы можете написать: {}help `команда` \nТак же, вы можете нажать на реакцию под сообщением, чтобы переключить страницу.\n'.format(prefix_in_guild))
+        emb.add_field(name='**Другая информация**',value='Чтобы получить больше информации о какой либо команде, вы можете написать: {}help `команда` \nТак же, вы можете нажать на реакцию под сообщением, чтобы переключить страницу.\n'.format(get_prefix))
         
-        emb1= discord.Embed(title='Команды информации', description='Что бы узнать больше о команде напишите {}help [команда]. \n**Пример**: {}help user'.format(prefix_in_guild,prefix_in_guild))
-        emb1.add_field(name='**Команды**', value=f'`{prefix_in_guild}user`\n`{prefix_in_guild}ping`\n`{prefix_in_guild}bot_servers`\n`{prefix_in_guild}tuser`\n')
+        emb1= discord.Embed(title='Команды информации', description='Что бы узнать больше о команде напишите {}help [команда]. \n**Пример**: {}help user'.format(get_prefix,get_prefix))
+        emb1.add_field(name='**Команды**', value=f'`{get_prefix}user`\n`{get_prefix}ping`\n`{get_prefix}bot_servers`\n`{get_prefix}tuser`\n')
         
-        emb2=discord.Embed(title='Команды администрации', description=f'`{prefix_in_guild}change_prefix` или `{prefix_in_guild}prefix`')
+        emb2=discord.Embed(title='Команды администрации', description=f'`{get_prefix}change_prefix` или `{get_prefix}prefix`')
 
         embeds=[emb,emb1,emb2]
         message= await ctx.send(embed= emb)
@@ -62,22 +56,22 @@ class HelpCommands(commands.Cog):
     
     @help_for_commands.command(name='user', aliases=['userinfo','ui','infouser','iu'])
     async def user_subcommands(self, ctx):
-        user_emb=discord.Embed(title=f'Информация про команду: {prefix_in_guild}user', colour = discord.Color.teal(), description=f'**Команда**: `[user]` или `[userinfo]` или `[infouser]` или `[iu]` или `[ui]`\n**Описание**: показивает информацию про пользователя\n**Использования**: `{prefix_in_guild}user` или `{prefix_in_guild}userinfo` или `{prefix_in_guild}infouser` или `{prefix_in_guild}iu` или `{prefix_in_guild}ui`, или если вы хотите узнать информацию о другом пользователя, то после команды пропишите тег пользователя о котором хотите узнать информацию\n**Пример**: `{prefix_in_guild}user @имя_пользователя`')
+        user_emb=discord.Embed(title=f'Информация про команду: {get_prefix}user', colour = discord.Color.teal(), description=f'**Команда**: `[user]` или `[userinfo]` или `[infouser]` или `[iu]` или `[ui]`\n**Описание**: показивает информацию про пользователя\n**Использования**: `{get_prefix}user` или `{get_prefix}userinfo` или `{get_prefix}infouser` или `{get_prefix}iu` или `{get_prefix}ui`, или если вы хотите узнать информацию о другом пользователя, то после команды пропишите тег пользователя о котором хотите узнать информацию\n**Пример**: `{get_prefix}user @имя_пользователя`')
         await ctx.send(embed=user_emb)
 
     @help_for_commands.command(name='ping')
     async def pind_subcommands(self,ctx):
-        ping_emb=discord.Embed(title=f'Информация про команду: {prefix_in_guild}ping',colour = discord.Color.teal(), description=f'**Команда**: `[ping]`\n**Описание**: показивает пинг бота\n**Использования**: `{prefix_in_guild}ping`')
+        ping_emb=discord.Embed(title=f'Информация про команду: {get_prefix}ping',colour = discord.Color.teal(), description=f'**Команда**: `[ping]`\n**Описание**: показивает пинг бота\n**Использования**: `{get_prefix}ping`')
         await ctx.send(embed=ping_emb)
 
     @help_for_commands.command(name='bot_servers')
     async def botservers_subcommands(self,ctx):
-        botservers_emb=discord.Embed(title=f'Информация про команду: {prefix_in_guild}bot_servers',colour = discord.Color.teal(), description=f'**Команда**: `[bot_servers]`\n**Описание**: показивает на сколько серверах присутствует этот бот\n**Использования**: `{prefix_in_guild}bot_servers`')
+        botservers_emb=discord.Embed(title=f'Информация про команду: {get_prefix}bot_servers', colour = discord.Color.teal(), description=f'**Команда**: `[bot_servers]`\n**Описание**: показивает на сколько серверах присутствует этот бот\n**Использования**: `{get_prefix}bot_servers`')
         await ctx.send(embed=botservers_emb)   
 
     @help_for_commands.command(name='tuser')
     async def tuser_subcommands(self,ctx):
-        tuser_emb=discord.Embed(title=f'Информация про команду: {prefix_in_guild}tuser',colour = discord.Color.teal(), description=f'**Команда**: `[tuser]`\n**Описание**: показивает сколько людей используют этого бота\n**Использования**: `{prefix_in_guild}tuser`')
+        tuser_emb=discord.Embed(title=f'Информация про команду: {get_prefix}tuser', colour = discord.Color.teal(), description=f'**Команда**: `[tuser]`\n**Описание**: показивает сколько людей используют этого бота\n**Использования**: `{get_prefix}tuser`')
         await ctx.send(embed=tuser_emb)   
 
 
