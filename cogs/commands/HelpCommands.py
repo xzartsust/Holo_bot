aa13babc9a6dc346c76075f2401c616ba3b9e353
xzartsust <igru1234567@gmail.com>
2020-08-13 22:01:50 +0300

    update

--- cogs/commands/HelpCommands.py
+++ cogs/commands/HelpCommands.py
@@ -22,7 +22,6 @@ conn = psycopg2.connect(
     port = "5432"
 )
 cursor = conn.cursor()
-
 def prefix_in_guild(bot, message):
     guildid = message.guild.id
     cursor.execute(f'SELECT prefix FROM public."prefixDB" WHERE guild_id = \'{guildid}\';')
@@ -30,17 +29,17 @@ def prefix_in_guild(bot, message):
     conn.commit()
     return prefix
 
-prefix = prefix_in_guild(bot,message)
-
 class HelpCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-
+    
+    
     @commands.group(name='help',aliases=['helpcmd','i','helpcommands'], invoke_without_command=True)
     async def help_for_commands(self, ctx):
         await ctx.channel.purge(limit=1)
-    
+
+        prefix = prefix_in_guild(self.bot, ctx.message)
 
         emb= discord.Embed(title=f'Команды бота {self.bot.user.name}', description='Здесь вы узнаете информацию про все команды бота\n')
         emb.add_field(name='**Другая информация**',value=f'Чтобы получить больше информации о какой либо команде, вы можете написать: {prefix}help `команда` \nТак же, вы можете нажать на реакцию под сообщением, чтобы переключить страницу.\n')
@@ -55,7 +54,7 @@ class HelpCommands(commands.Cog):
         page= pag(self.bot, message, only=ctx.author, use_more=False, embeds=embeds, color=0x008000, time_stamp=True)
     
         await page.start()
-    
+    '''
     @help_for_commands.command(name='user', aliases=['userinfo','ui','infouser','iu'])
     async def user_subcommands(self, ctx):
         user_emb=discord.Embed(title=f'Информация про команду: {get_prefix}user', colour = discord.Color.teal(), description=f'**Команда**: `[user]` или `[userinfo]` или `[infouser]` или `[iu]` или `[ui]`\n**Описание**: показивает информацию про пользователя\n**Использования**: `{get_prefix}user` или `{get_prefix}userinfo` или `{get_prefix}infouser` или `{get_prefix}iu` или `{get_prefix}ui`, или если вы хотите узнать информацию о другом пользователя, то после команды пропишите тег пользователя о котором хотите узнать информацию\n**Пример**: `{get_prefix}user @имя_пользователя`')
@@ -75,6 +74,6 @@ class HelpCommands(commands.Cog):
     async def tuser_subcommands(self,ctx):
         tuser_emb=discord.Embed(title=f'Информация про команду: {get_prefix}tuser', colour = discord.Color.teal(), description=f'**Команда**: `[tuser]`\n**Описание**: показивает сколько людей используют этого бота\n**Использования**: `{get_prefix}tuser`')
         await ctx.send(embed=tuser_emb)   
-
+'''
 def setup(bot):
     bot.add_cog(HelpCommands(bot))
