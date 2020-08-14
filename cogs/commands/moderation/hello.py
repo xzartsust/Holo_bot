b1a7fc90547b15d4a32822a7354da5d005362fd1
xzartsust <igru1234567@gmail.com>
2020-08-14 21:46:15 +0300

    Create hello.py

---
+++ cogs/commands/moderation/hello.py
@@ -0,0 +1,39 @@
+import discord
+from discord.ext import commands
+import asyncpg, asyncio
+import psycopg2
+import os
+
+database = os.environ.get('DATABASE')
+user = os.environ.get('USER')
+password = os.environ.get('PASSWORD')
+host = os.environ.get('HOST')
+port = os.environ.get('PORT')
+
+conn = psycopg2.connect(
+    database = f"{database}", 
+    user = f"{user}", 
+    password = f"{password}", 
+    host = f"{host}", 
+    port = "5432"
+)
+
+cursor = conn.cursor()
+
+class member_greeting(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_guild_join(self, ctx):
+        pass
+
+
+    @commands.command()
+    async def greet(self, ctx, channel: discord.TextChannel):
+        channel_for_greet = ctx.message.guild.id
+        cursor.execute(f'UPDATE public."prefixDB" SET channel_for_greet=\'{channel}\' WHERE guild_id = \'{channel_for_greet}\';')
+        conn.commit()
+
+def setup(bot):
+    bot.add_cog(member_greeting(bot))
