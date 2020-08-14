152284e528743c089a86772fb6da7e7f1587d186
xzartsust <igru1234567@gmail.com>
2020-08-14 17:50:50 +0300

    update

--- cogs/commands/info/invite.py
+++ cogs/commands/info/invite.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from Cybernator import Paginator as pag
 
 
 class invite(commands.Cog):
@@ -11,12 +12,19 @@ class invite(commands.Cog):
         
         emb = discord.Embed(
             timestamp = ctx.message.created_at,
-            title = f'Мои силки[https://discord.com/api/oauth2/authorize?client_id=729957701240750140&permissions=2147483639&scope=bot]',
-            description = '',
+            title = f'Мои силки',
+            description = 'Так же, вы можете нажать на реакцию под сообщением, чтобы получить больше силок',
         )
-        #emb.
+        emb1 = discord.Embed(
+            title='Мой инвайт',
+            url='https://discord.com/api/oauth2/authorize?client_id=729957701240750140&permissions=2147483639&scope=bot'
+        )
+        embeds = [emb,emb1]
+        message = await ctx.send(embed= emb)
+        page = pag(self.bot, message, only=ctx.author, use_more=False, embeds=embeds, color=0xFFFF00, time_stamp=True)
+    
+        await page.start()
         
-        await ctx.send(embed= emb)
 
 
 def setup(bot):
