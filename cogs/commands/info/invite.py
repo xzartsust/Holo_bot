0b2e4be05c7d17ecc32cf75a57e4b75ecea2233d
xzartsust <igru1234567@gmail.com>
2020-08-14 16:56:48 +0300

    update

--- cogs/commands/info/invite.py
+++ cogs/commands/info/invite.py
@@ -6,7 +6,15 @@ class invite(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-
+    @commands.command()
+    async def invite(self, ctx):
+        
+        emb = discord.Embed(
+            timestamp = ctx.message.created_at,
+            title = 'Мои силки',
+            description = '',
+        )
+        emb.set_thumbnail(url='https://discord.com/api/oauth2/authorize?client_id=729957701240750140&permissions=2147483639&scope=bot')
 
 
 def setup(bot):
