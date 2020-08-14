820bd190b2d71894e96afb35599740d6f797c94b
xzartsust <igru1234567@gmail.com>
2020-08-14 13:33:19 +0300

    update

--- cogs/commands/clear.py
+++ cogs/commands/clear.py
@@ -9,13 +9,17 @@ class clear(commands.Cog):
     @commands.has_permissions(administrator=True)
     async def clear(self, ctx, arg: int):
         await ctx.channel.purge(limit= arg + 1)
-'''
+
     @clear.error
     async def clear_error(self, ctx, error):
         if isinstance(error, commands.MissingPermissions):
-            emb = discord.Embed(title='Ошибка!!!', colour=discord.Color.red(), description='У вас нет прав на ету команду')
+            emb = discord.Embed(timestamp= ctx.message.created_at, title='Ошибка!!!', colour=discord.Color.red(), description='У вас нет прав на ету команду')
+            emb.set_footer(text= ctx.message.author)
             await ctx.channel.purge(limit=1)
             await ctx.send(embed=emb)
-'''
+        if isinstance(error, commands.MissingRequiredArgument):
+            emb = discord.Embed(title='Ошибка!!!', colour=discord.Color.red(), description='Укажите сколько вы хотите удалить сообщений')
+            await ctx.channel.purge(limit=1)
+
 def setup(bot):
     bot.add_cog(clear(bot))
