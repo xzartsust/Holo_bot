3b096c477337e9109ffcc855624c48e8ddca2290
xzartsust <igru1234567@gmail.com>
2020-08-17 17:32:18 +0300

    Update on_guild_join.py

--- cogs/bot_event/on_guild_join.py
+++ cogs/bot_event/on_guild_join.py
@@ -27,7 +27,7 @@ class bot_join_guild(commands.Cog):
 
     @commands.Cog.listener()
     async def on_guild_join(self, guild): 
-        cursor.execute(f'INSERT INTO public."prefixDB" (guild_name, guild_id, prefix) VALUES ({guild.name}, {guild.id}, \'.\');')
+        cursor.execute(f'INSERT INTO public."prefixDB" (guild_name, guild_id, prefix) VALUES (\'{guild.name}\', {guild.id}, \'.\');')
         conn.commit()
         
 def setup(bot):
