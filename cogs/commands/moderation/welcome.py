6d687fcb36b5e89c6301b12ecf3771e8f7ec09ed
xzartsust <igru1234567@gmail.com>
2020-08-17 20:01:53 +0300

    Update welcome.py

--- cogs/commands/moderation/welcome.py
+++ cogs/commands/moderation/welcome.py
@@ -27,7 +27,7 @@ class member_greeting(commands.Cog):
         self.bot = bot
 
     @commands.Cog.listener()
-    async def on_member_join(self,ctx, membere):
+    async def on_member_join(self, ctx, member):
         join_guild_id = ctx.guild.id
         cursor.execute(f'SELECT channel_for_greeting FROM public."prefixDB" WHERE guild_id = \'{join_guild_id}\';')
         chan = cursor.fetchone()
