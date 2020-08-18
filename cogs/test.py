62ceff0753aadc511b5778ab1eca47365b6b7bc8
xzartsust <igru1234567@gmail.com>
2020-08-18 21:55:32 +0300

    update

--- cogs/test.py
+++ cogs/test.py
@@ -1,10 +1,8 @@
 import discord
 from discord.ext import commands
 import time
-start = time.time()
-finish = time.time()
-result = finish - start
-print("Program time: " + str(result) + " seconds.")
+start = time.monotonic()
+result = time.monotonic() - start
 
 
 class Test(commands.Cog):
@@ -14,7 +12,7 @@ class Test(commands.Cog):
     @commands.command()
     @commands.is_owner()
     async def test(self, ctx):
-        await ctx.send(str(result))
+        await ctx.send("Program time: {:>.3f}".format(result) + " seconds.")
         
 def setup(bot):
     bot.add_cog(Test(bot))
