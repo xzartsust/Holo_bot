1318f35571208d46bb320a62b3a8d5553eb29328
xzartsust <igru1234567@gmail.com>
2020-08-18 21:51:42 +0300

    update

--- cogs/commands/info/infobot.py
+++ cogs/commands/info/infobot.py
@@ -1,12 +1,10 @@
 import discord
 from discord.ext import commands
 
-
 class InfoBot(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    
 
 def setup(bot):
     bot.add_cog(InfoBot(bot))
--- cogs/test.py
+++ cogs/test.py
@@ -1,5 +1,11 @@
 import discord
 from discord.ext import commands
+import time
+start = time.time()
+finish = time.time()
+result = finish - start
+print("Program time: " + str(result) + " seconds.")
+
 
 class Test(commands.Cog):
     def __init__(self,bot):
@@ -8,8 +14,7 @@ class Test(commands.Cog):
     @commands.command()
     @commands.is_owner()
     async def test(self, ctx):
-        e = discord.Embed(title='foo')
-        await ctx.send('Hello', embed=e)
+        await ctx.send(str(result))
         
 def setup(bot):
     bot.add_cog(Test(bot))
