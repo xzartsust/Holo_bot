d5682a1f2b354c7295efcb8c879d6fd89f76fef2
xzartsust <igru1234567@gmail.com>
2020-08-18 22:26:19 +0300

    update

--- cogs/commands/info/infobot.py
+++ cogs/commands/info/infobot.py
@@ -5,6 +5,16 @@ class InfoBot(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.command()
+    async def infobot(self, ctx):
+        await ctx.channel.purge(limit = 1)
+        embed=discord.Embed(title="Инфорация про бота\n ")
+        embed.add_field(name="Библиотека", value="discord.py", inline=False)
+        embed.add_field(name="Состояние", value="Beta", inline=False)
+        embed.add_field(name="Версия", value="0.0.3 Beta", inline=True)
+        embed.add_field(name="Сайт", value="Скоро...", inline=True)
+        embed.add_field(name="Support server", value="https://discord.gg/8f4KUp", inline=False)
+        await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(InfoBot(bot))
--- cogs/test.py
+++ cogs/test.py
@@ -2,18 +2,19 @@ import discord
 from discord.ext import commands
 import time
 
+start = time.monotonic()
+
 
 
 class Test(commands.Cog):
     def __init__(self,bot):
         self.bot = bot
-        self.start = time.monotonic()
-        self.result = time.monotonic() - self.start
-    
+        
     @commands.command()
     @commands.is_owner()
     async def test(self, ctx):
-        await ctx.send("Program time: {:>.3f}".format(self.result) + " seconds.")
+        result = time.monotonic() - start
+        await ctx.send("Program time: {:>.3f}".format(result) + " seconds.")
         
 def setup(bot):
     bot.add_cog(Test(bot))
