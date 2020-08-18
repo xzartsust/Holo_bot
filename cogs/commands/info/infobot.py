d2535abbdcc1377839d7dd9ea756c645f33b161f
xzartsust <igru1234567@gmail.com>
2020-08-18 23:12:50 +0300

    Update infobot.py

--- cogs/commands/info/infobot.py
+++ cogs/commands/info/infobot.py
@@ -1,5 +1,9 @@
 import discord
 from discord.ext import commands
+import time
+import datetime as DT
+
+start = time.monotonic()
 
 class InfoBot(commands.Cog):
     def __init__(self, bot):
@@ -8,12 +12,42 @@ class InfoBot(commands.Cog):
     @commands.command()
     async def infobot(self, ctx):
         await ctx.channel.purge(limit = 1)
-        embed=discord.Embed(title="Инфорация про бота\n ")
-        embed.add_field(name="Библиотека", value="discord.py", inline=False)
-        embed.add_field(name="Состояние", value="Beta", inline=False)
-        embed.add_field(name="Версия", value="0.0.3 Beta", inline=True)
-        embed.add_field(name="Сайт", value="Скоро...", inline=True)
-        embed.add_field(name="Support server", value="https://discord.gg/8f4KUp", inline=False)
+
+        result = time.monotonic() - start
+
+        embed=discord.Embed(
+            title="Инфорация про бота\n ",
+            timestamp = ctx.message.created_at
+        )
+        embed.add_field(
+            name="Библиотека",
+            value="discord.py",
+            inline=False
+        )
+        embed.add_field(
+            name="Состояние",
+            value="Beta", 
+            inline=False
+        )
+        embed.add_field(
+            name="Версия",
+            value="0.0.3 Beta",
+            inline=True
+        )
+        embed.add_field(
+            name="Сайт",
+            value="Скоро...",
+            inline=True
+        )
+        embed.add_field(
+            name="Support server",
+            value="https://discord.gg/8f4KUp",
+            inline=False
+        )
+        embed.add_field(
+            name='Время с последнего запуска'
+            value= DT.timedelta(seconds=result
+        )
         await ctx.send(embed=embed)
 
 def setup(bot):
