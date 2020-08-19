5c9c5aa6f38899cf4a8501e728400aeedbad6f6f
xzartsust <igru1234567@gmail.com>
2020-08-19 15:27:19 +0300

    Update test.py

--- cogs/test.py
+++ cogs/test.py
@@ -6,7 +6,13 @@ class Test(commands.Cog):
     def __init__(self,bot):
         self.bot = bot
         
-
+    @commands.command()
+    @commands.is_owner()
+    async def test(self, ctx):
+        i = len(ctx.message.guild.features)
+        for i in range(0, i+1):
+            while i < len(ctx.message.guild.features):
+                print('1')
         
 def setup(bot):
     bot.add_cog(Test(bot))
