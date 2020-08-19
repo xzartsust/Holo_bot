c56fbc906d9483d1867c3e5ca9e09852c36898e0
xzartsust <igru1234567@gmail.com>
2020-08-19 14:42:43 +0300

    Update serverinfo.py

--- cogs/commands/info/serverinfo.py
+++ cogs/commands/info/serverinfo.py
@@ -7,23 +7,6 @@ from discord import utils
 from discord.utils import get
 
 
-database = os.environ.get('DATABASE')
-user = os.environ.get('USER')
-password = os.environ.get('PASSWORD')
-host = os.environ.get('HOST')
-port = os.environ.get('PORT')
-
-conn = psycopg2.connect(
-    database = f"{database}", 
-    user = f"{user}", 
-    password = f"{password}", 
-    host = f"{host}", 
-    port = "5432"
-)
-
-cursor = conn.cursor()
-
-
 class ServerInfo(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -31,7 +14,29 @@ class ServerInfo(commands.Cog):
 
     @commands.command(aliases = ['si','is'])
     async def serverinfo(self, ctx):
-        pass
+        guild = ctx.message.guild
+
+        embed = discord.Embed(
+            title = '',
+            colour = discord.Color.orange(),
+            timestamp = ctx.message.created_at
+        )
+        embed.add_field(
+            name = 'Owner сервера',
+            value = guild.owner.mention,
+            inline = False
+        )
+        embed.add_field(
+            name = 'Названия сервера',
+            value = guild.name,
+            inline = False
+        )
+
+
+
+        await ctx.send(embed = embed)
+
+
 
 
 def setup(bot):
