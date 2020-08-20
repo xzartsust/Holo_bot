384f6239099bf09637d11ebe8297caefe7155524
xzartsust <igru1234567@gmail.com>
2020-08-20 21:12:07 +0300

    update

--- cogs/commands/info/help_commands.py
+++ cogs/commands/info/help_commands.py
@@ -63,7 +63,7 @@ class HelpCommands(commands.Cog):
         )
         emb2.add_field(
             name = '**Команды**',
-            value = f'`{prefix}prefix`\n`{prefix}wlc` или `{prefix}welcome`\n`{prefix}news`'
+            value = f'`{prefix}prefix`\n`{prefix}wlc` или `{prefix}welcome`\n`{prefix}news`\n`{prefix}rwlc`'
         )
         emb3=discord.Embed(
             title='Команды для развлечения', 
@@ -255,6 +255,23 @@ class HelpCommands(commands.Cog):
             icon_url = ctx.message.author.avatar_url
         )
         await ctx.send(embed=news_emb) 
+
+    @help_for_commands.command(name='rwlc')
+    async def rwlc_subcommands(self, ctx):
+        prefix_1 = prefix_in_guild(self.bot, ctx.message)
+        prefix = prefix_1[0]
+
+        wlc_emb=discord.Embed(
+            timestamp= ctx.message.created_at, 
+            title=f'Информация про команду: `{prefix}rwlc`', 
+            colour = discord.Color.teal(), 
+            description=f'**Предостережение:** Эту команду может использовать только создатель сервера!\n**Команда**: `[rwlc]`\n**Описание**: установить роль которая будет выдаваться новим пользователям сервера\n**Использования**: `{prefix}rwlc *ади роли* *true или false*`\n**true** - включить автовидачу роли\n**false** - отключить автовидачу роли\n\n**Пример:** `{prefix}rwlc 112215155842828482 true`'
+        )
+        wlc_emb.set_footer(
+            text = ctx.message.author,
+            icon_url = ctx.message.author.avatar_url
+        )
+        await ctx.send(embed=wlc_emb)     
 
 def setup(bot):
     bot.add_cog(HelpCommands(bot))
