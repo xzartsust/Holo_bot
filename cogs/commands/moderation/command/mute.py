b832c87813ab03a71987ef4db74b1d6a2646d0fb
xzartsust <igru1234567@gmail.com>
2020-08-21 11:09:17 +0300

    Update mute.py

--- cogs/commands/moderation/command/mute.py
+++ cogs/commands/moderation/command/mute.py
@@ -10,6 +10,7 @@ class MuteCommand(commands.Cog):
         self.bot = bot
 
     @commands.command()
+    @commands.has_permissions(administrator = True)
     async def mute(self, ctx, who: discord.Member, time: int, reason):
         role = ctx.message.guild.get_role(746275532039258122)
         print(role)
@@ -19,6 +20,7 @@ class MuteCommand(commands.Cog):
         await who.move_to(None)
         await asyncio.sleep(time * 60)
         await who.remove_roles(role)
+        await ctx('Мут забраний')
 
 def setup(bot):
     bot.add_cog(MuteCommand(bot))
