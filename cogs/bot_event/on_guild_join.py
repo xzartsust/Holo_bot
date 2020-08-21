87ee794b61dc5b98b47015b492f82f60a1ef921c
xzartsust <igru1234567@gmail.com>
2020-08-21 15:10:51 +0300

    update

--- cogs/bot_event/on_guild_join.py
+++ cogs/bot_event/on_guild_join.py
@@ -29,7 +29,11 @@ class bot_join_guild(commands.Cog):
     async def on_guild_join(self, guild): 
         cursor.execute(f'INSERT INTO public."prefixDB" (guild_id, prefix) VALUES ({guild.id}, \'t!\');')
         conn.commit()
+        
         cursor.execute(f'INSERT INTO public.giveroles(guild_id) VALUES ({guild.id});')
+        conn.commit()
+        
+        cursor.execute(f'INSERT INTO public.mute_role(guild_id) VALUES ({guild.id});')
         conn.commit()
         
 def setup(bot):
--- cogs/commands/moderation/command/mute.py
+++ cogs/commands/moderation/command/mute.py
@@ -4,6 +4,27 @@ import psycopg2
 import asyncio, asyncpg
 import os
 
+########################################################## Connect to SQL ###################################################
+
+
+database = os.environ.get('DATABASE')
+user = os.environ.get('USER')
+password = os.environ.get('PASSWORD')
+host = os.environ.get('HOST')
+port = os.environ.get('PORT')
+
+conn = psycopg2.connect(
+    database = f"{database}", 
+    user = f"{user}", 
+    password = f"{password}", 
+    host = f"{host}", 
+    port = "5432"
+)
+
+cursor = conn.cursor()
+
+
+########################################################################################################################
 
 class MuteCommand(commands.Cog):
     def __init__(self, bot):
@@ -11,15 +32,23 @@ class MuteCommand(commands.Cog):
 
     @commands.command()
     @commands.has_permissions(administrator = True)
-    async def mute(self, ctx, who: discord.Member, time: int, what: str, reason):
-        role = ctx.message.guild.get_role(746275532039258122)
+    async def mute(self, ctx, who: discord.Member, time: int, what: str, reason = None):
         
-        print(f'[command.mute] От {ctx.author}, кого {who}')
+        guild = ctx.message.guild
+        cursor.execute(f'SELECT role_id FROM public.mute_role WHERE guild_id = \'{guild.id}\';')
+        role_mute = cursor.fetchone()
+        conn.commit()
+        
+        role = ctx.message.guild.get_role(role_mute[0])
+        
+        await ctx.channel.purge(limit = 1)
+        
+        print(f'[command.mute] От {ctx.author}, кого {who} роль {role}')
         
         if what == str('m'):
             if time >=1 and time <= 59:
                 
-                await ctx.send(f'--> {who} получил мут на {time} минут по причине: {reason}')
+                await ctx.send(f'--> {who} получил мут на {time}м. по причине: {reason}')
 
                 await who.add_roles(role)
                 await who.move_to(None)
@@ -29,7 +58,7 @@ class MuteCommand(commands.Cog):
         
         if what == str('h'):
             if time >= 1 and time <= 24:
-                await ctx.send(f'--> {who} получил мут на {time} часов по причине: {reason}')
+                await ctx.send(f'--> {who} получил мут на {time}ч. по причине: {reason}')
 
                 await who.add_roles(role)
                 await who.move_to(None)
@@ -39,7 +68,7 @@ class MuteCommand(commands.Cog):
         
         if what == str('d'):
             if time >= 1 and time <= 365:
-                await ctx.send(f'--> {who} получил мут на {time} дней по причине: {reason}')
+                await ctx.send(f'--> {who} получил мут на {time}д. по причине: {reason}')
 
                 await who.add_roles(role)
                 await who.move_to(None)
@@ -49,13 +78,38 @@ class MuteCommand(commands.Cog):
         
         if what == str('y'):
             if time >= 1:
-                await ctx.send(f'--> {who} получил мут на {time} лет по причине: {reason}')
+                await ctx.send(f'--> {who} получил мут на {time}л. по причине: {reason}')
 
                 await who.add_roles(role)
                 await who.move_to(None)
-                await asyncio.sleep(time * 1557600)
+                await asyncio.sleep(time * 31557600)
                 await who.remove_roles(role)
                 await ctx.send('Мут забраний') 
+
+
+    @commands.command()
+    @commands.has_permissions(administrator = True)
+    async def muterole(self, ctx, role_id: int):
+        guild = ctx.message.guild
+        role = ctx.message.guild.get_role(role_id)
+
+        cursor.execute(f'UPDATE public.mute_role SET role_id = \'{role_id}\' WHERE guild_id = \'{guild.id}\';')
+        conn.commit()
+
+        emb = discord.Embed(
+            title= 'Успешно!',
+            description = f'Роль {role.mention} была установленна для команды `mute`',
+            timestamp = ctx.message.created_at
+            )
+        emb.set_footer(
+            text = 'Запросил: ' + ctx.author,
+            icon_url = ctx.author.avatar_url
+            )
+        
+        if ctx.guild.system_channel is not None:
+            await ctx.guild.system_channel.send(embed = emb)
+        elif ctx.guild.system_channel is None:
+            await ctx.send(embed = emb)
 
 def setup(bot):
     bot.add_cog(MuteCommand(bot))
