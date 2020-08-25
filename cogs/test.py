6ffdacf34313b8a00c4be77193d1e4c43fc051bf
xzartsust <igru1234567@gmail.com>
2020-08-25 10:34:23 +0300

    Update test.py

--- cogs/test.py
+++ cogs/test.py
@@ -16,6 +16,8 @@ class Test(commands.Cog):
         guildid = сtx.message.guild
         allvoice = guildid.voice_channels
         alltext = guildid.text_channels
+        print(allvoice)
+        print(alltext)
         await alltext.set_permissions(role, read_messages = True, send_messages = True, manage_channels = True, manage_roles = True)
         await allvoice.set_permissions(role, connect = True, manage_channels = True, manage_roles = True)
         await сtx.send(f'{сtx.author.mention}, вы успешно установили {role.mention} права доступа во всех текстовых/голосовых каналах')
