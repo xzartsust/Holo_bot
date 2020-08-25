4b4349b5692f3be83d77f5b18cdcab929e3e918f
xzartsust <igru1234567@gmail.com>
2020-08-25 10:50:36 +0300

    Update test.py

--- cogs/test.py
+++ cogs/test.py
@@ -12,14 +12,12 @@ class Test(commands.Cog):
         
     @commands.command()
     @commands.is_owner()
-    async def test(self, сtx, role: discord.Role = None):
+    async def test(self, сtx, role: discord.Role = None, name = None):
         guildid = сtx.message.guild
-        allvoice = guildid.voice_channels[0]
+        allvoice = guildid.voice_channels[name]
         alltext = guildid.text_channels[0]
-        print(allvoice)
-        print(alltext)
-        await role.set_permissions(role, read_messages = True, send_messages = True, manage_channels = True, manage_roles = True)
-        #await allvoice.set_permissions(role, connect = True, manage_channels = True, manage_roles = True)
+        await alltext.set_permissions(role, read_messages = True, send_messages = True, manage_channels = True, manage_roles = True)
+        await allvoice.set_permissions(role, connect = True, manage_channels = True, manage_roles = True)
         await сtx.send(f'{сtx.author.mention}, вы успешно установили {role.mention} права доступа во всех текстовых/голосовых каналах')
 
 def setup(bot):
