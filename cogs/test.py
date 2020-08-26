dffeb2da5007d98044f3e01a39aa8d5995ed6e01
xzartsust <igru1234567@gmail.com>
2020-08-26 15:56:38 +0300

    Update test.py

--- cogs/test.py
+++ cogs/test.py
@@ -5,6 +5,7 @@ import json
 from discord.utils import get
 import asyncio
 import aiohttp
+import io
 
 
 class Test(commands.Cog):
@@ -15,9 +16,10 @@ class Test(commands.Cog):
     @commands.is_owner()
     async def test(self, ctx):
         async with aiohttp.ClientSession() as session:
-            async with session.get('https://nekos.life') as r:
-                if r.status == 200:
-                    js = await r.json()
-                    await ctx.send(js['file'])
+            async with session.get('https://nekos.life') as resp:
+                if resp.status != 200:
+                    return await ctx.send('Could not download file...')
+                data = io.BytesIO(await resp.read())
+                await ctx.send(file=discord.File(data, 'cool_image.png'))
 def setup(bot):
     bot.add_cog(Test(bot))
