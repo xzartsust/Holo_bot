914ae5e4047a789915f8e51fd448794678a021f9
xzartsust <igru1234567@gmail.com>
2020-09-01 12:33:02 +0300

    update

--- cogs/test.py
+++ cogs/test.py
@@ -15,11 +15,7 @@ class Test(commands.Cog):
     @commands.command()
     @commands.is_owner()
     async def test(self, ctx):
-        async with aiohttp.ClientSession() as session:
-            async with session.get('https://nekos.life') as resp:
-                if resp.status != 200:
-                    return await ctx.send('Could not download file...')
-                data = io.IOBase(await resp.read())
-                await ctx.send(file=discord.File(data, 'cool_image.png'))
+        pass
+    
 def setup(bot):
     bot.add_cog(Test(bot))
