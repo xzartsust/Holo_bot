cccf23b4a4f76d00a4934e88eb8f1365b98e0706
xzartsust <igru1234567@gmail.com>
2020-09-03 18:57:34 +0300

    Update infobot.py

--- cogs/commands/info/infobot.py
+++ cogs/commands/info/infobot.py
@@ -1,5 +1,7 @@
 import discord
 from discord.ext import commands
+from discord import utils
+from discord.utils import get
 import os
 
 
@@ -12,6 +14,11 @@ class InfoBot(commands.Cog):
         
         await ctx.channel.purge(limit = 1)
 
+        d = self.bot.set_user(bot_owner)
+        
+        print('Bot_owner: ', bot_owner)
+        print(d)
+
         embed=discord.Embed(
             title=":information_source: BOT INFORMATION :information_source:",
             timestamp = ctx.message.created_at,
@@ -22,27 +29,27 @@ class InfoBot(commands.Cog):
             value = f'```{self.bot.get_user(bot_owner)}```'
         )
         embed.add_field(
-            name="Библиотека",
+            name="Library",
             value="```discord.py```",
             inline=False
         )
         embed.add_field(
-            name="Состояние",
+            name="Status",
             value="```Beta```", 
             inline=False
         )
         embed.add_field(
-            name="Версия",
+            name="Version",
             value="```0.0.3 Beta```",
             inline=True
         )
         embed.add_field(
-            name="Сайт",
+            name="Website",
             value="https://github.com/xzartsust/Tobi-Bot",
             inline=False
         )
         embed.add_field(
-            name="Support server",
+            name="Official Bot Support Server",
             value="https://discord.gg/8f4KUp",
             inline=False
         )
