6b90dd14202779db508746e34d5bfaf6fd67bcb7
xzartsust <igru1234567@gmail.com>
2020-09-04 22:20:49 +0300

    Update play.py

--- cogs/commands/music/play.py
+++ cogs/commands/music/play.py
@@ -27,17 +27,17 @@ class MusicPlay(commands.Cog):
         def endSong(self, guild, path):
             os.remove(path)
 
-        guild_id = ctx.message.guild.id 
+        guild_id = ctx.message.guild.id
+
+        channel_id = ctx.message.author.voice.channel
+
+        channel1 = self.bot.get_channel(channel_id.id)                   
+        voice_client = await channel1.connect()
         
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             file = ydl.extract_info(url, download=True)
             guild = guild_id 
-            path = str(file['title']) + "-" + str(file['id'] + ".mp3")
-        
-        channel_id = ctx.message.author.voice.channel
-
-        channel1 = self.bot.get_channel(channel_id.id)                   
-        voice_client = await channel1.connect()                                     
+            path = str(file['title']) + "-" + str(file['id'] + ".mp3")                                     
         
         voice_client.play(discord.FFmpegPCMAudio(path), after=lambda x: endSong(self, guild, path))
         voice_client.source = discord.PCMVolumeTransformer(voice_client.source, 1)
