b44fa5fb674d68537cbceb3807f9705bc3b64bf4
xzartsust <igru1234567@gmail.com>
2020-09-09 21:15:45 +0300

    Update logs_guild.py

--- cogs/commands/moderation/logs_guild.py
+++ cogs/commands/moderation/logs_guild.py
@@ -6,7 +6,9 @@ class GuildLogs(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    
+    @commands.Cog.listener()
+    async for entry in guild.audit_logs(action = discord.AuditLogAction.channel_update):
+        print('1')
 
 def setup(bot):
     bot.add_cog(GuildLogs(bot))
