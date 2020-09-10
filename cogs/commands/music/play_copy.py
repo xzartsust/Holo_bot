261d6ccfa1c1448e5fbc03dd949559d4b27f3c0e
xzartsust <igru1234567@gmail.com>
2020-09-10 20:56:53 +0300

    update

--- cogs/commands/music/play_copy.py
+++ cogs/commands/music/play_copy.py
@@ -5,161 +5,30 @@ import youtube_dl
 import os
 import shutil
 
-
+players = {}
 
 class MusicPlay(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-    
-    
-    
+
     @commands.command()
-    async def play(self, ctx, url: str):
-        
-        voice = get(self.bot.voice_clients, guild=ctx.guild)   
+    async def join(self, ctx):
+        channel = ctx.message.author.voice.voice_channel
+        await channel.connect()
 
-        def check_queue():
-            Queue_infile = os.path.isdir(".\\Queue")
-            if Queue_infile is True:
-                DIR = os.path.abspath(os.path.realpath("Queue"))
-                length = len(os.listdir(DIR))
-                still_q = length - 1
-                try:
-                    first_file = os.listdir(DIR)[0]
-                except:
-                    print("No more queued song(s)\n")
-                    queues.clear()
-                    return
-                
-                main_location = os.path.dirname(os.path.realpath(__file__))
-                song_path = os.path.abspath(os.path.realpath("Queue") + "\\" + first_file)
-                if length != 0:
-                    print("Song done, playing next queued\n")
-                    print(f"Songs still in queue: {still_q}")
-                    song_there = os.path.isfile("song.mp3")
-                    if song_there:
-                        os.remove("song.mp3")
-                    shutil.move(song_path, main_location)
-                    for file in os.listdir(".\\"):
-                        if file.endswith(".mp3"):
-                            os.rename(file, 'song.mp3')
-                            
-                    voice.play(discord.FFmpegPCMAudio("song.mp3"), after=lambda e: check_queue())
-                    voice.source = discord.PCMVolumeTransformer(voice.source)
-                    voice.source.volume = 0.07
+    @commands.command()
+    async def leave(self, ctx):
+        voice = get(self.bot.voice_clients, guild = ctx.guild)
+        await voice.disconnect()
 
-                else:
-                    queues.clear()
-                    return
+    @commands.command()
+    async def play(self, ctx, url):
+        guild = ctx.message.guild
+        voice = get(self.bot.voice_client, guild = ctx.guild)
+        player = await voice.create_ytdl_player(url)
+        players[guild.id] = player
+        player.start()
 
-            else:
-                queues.clear()
-                print("No songs were queued before the ending of the last song\n")
-
-        song_there = os.path.isfile("song.mp3")
-        try:
-            if song_there:
-                os.remove("song.mp3")
-                queues.clear()
-                print("Removed old song file")
-        except PermissionError:
-            print("Trying to delete song file, but it's being played")
-            await ctx.send("ERROR: Music playing")
-            return
-            
-        Queue_infile = os.path.isdir(".\\Queue")
-        
-        try:
-            Queue_folder = ".\\Queue"
-            if Queue_infile is True:
-                print("Removed old Queue Folder")
-                shutil.rmtree(Queue_folder)
-        except:
-            print("No old Queue folder")
-            
-        await ctx.send("Getting everything ready now")
-
-        ydl_opts = {
-            'format': 'bestaudio/best',
-            'quiet': True,
-            'postprocessors': [{
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'mp3',
-                'preferredquality': '192',
-            }],
-        }
-
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            print("Downloading audio now\n")
-            ydl.download([url])
-            
-        for file in os.listdir("./"):
-            if file.endswith(".mp3"):
-                name = file
-                print(f"Renamed File: {file}\n")
-                os.rename(file, "song.mp3")
-
-        voice.play(discord.FFmpegPCMAudio("song.mp3"), after=lambda e: check_queue())
-        voice.source = discord.PCMVolumeTransformer(voice.source)
-        voice.source.volume = 0.07
-        
-        nname = name.rsplit("-", 2)
-        await ctx.send(f"Playing: {nname[0]}")
-        print("playing\n")
-
-    @commands.command(aliases=['q', 'que'])
-    async def queue(self, ctx, url: str):
-
-        global queues
-        queues = {}
-
-        Queue_infile = os.path.isdir(".\\Queue")
-            
-        if Queue_infile is False:
-            os.mkdir("Queue")
-            
-        DIR = os.path.abspath(os.path.realpath("Queue"))
-        q_num = len(os.listdir(DIR))
-        q_num += 1
-        add_queue = True
-            
-        while add_queue:
-            if q_num in queues:
-                q_num += 1
-            else:
-                add_queue = False
-                queues[q_num] = q_num
-            
-        queue_path = os.path.abspath(os.path.realpath("Queue") + f"\\song{q_num}.%(ext)s")
-
-        ydl_opts = {
-            'format': 'bestaudio/best',
-            'quiet': True,
-            'outtmpl': queue_path,
-            'postprocessors': [{
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'mp3',
-                'preferredquality': '192',
-            }],
-        }
-            
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            print("Downloading audio now\n")
-            ydl.download([url])
-            
-        await ctx.send("Adding song " + str(q_num) + " to the queue")
-        print("Song added to queue\n")
-    
-    async def next(self, ctx):
-        voice = get(self.bot.voice_clients, guild=ctx.guild)
-        
-        if voice and voice.is_playing():
-            print("Playing Next Song")
-            voice.stop()
-            await ctx.send("Next Song")
-        else:
-            print("No music playing")
-            await ctx.send("No music playing failed")
 
 def setup(bot):
     bot.add_cog(MusicPlay(bot))
