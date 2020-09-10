79bca72ac2b7c1ec41707dab5f8af62fe73d17ac
xzartsust <igru1234567@gmail.com>
2020-09-10 22:53:16 +0300

    Update ping.py

--- cogs/commands/info/ping.py
+++ cogs/commands/info/ping.py
@@ -5,12 +5,12 @@ import time
 class ping_serv(commands.Cog):
     def __init__(self,bot):
         self.bot = bot
-        self.ping = bot.latency
+        self._ping = bot.latency
 
     @commands.command()
     async def ping(self, ctx):
 
-        emb= discord.Embed(description = ':ping_pong:Pong: {0.ping}ms'.format(self), colour=discord.Color.blurple())
+        emb= discord.Embed(description = ':ping_pong:Pong: {0._ping}ms'.format(self), colour=discord.Color.blurple())
         emb.set_footer(text = ctx.message.author)
         await ctx.send(embed = emb)
 
