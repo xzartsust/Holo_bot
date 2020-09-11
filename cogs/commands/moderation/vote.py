cdd5d4ffb0ee1c9916419889c71c21cd246bcaed
xzartsust <igru1234567@gmail.com>
2020-09-12 00:40:10 +0300

    Update vote.py

--- cogs/commands/moderation/vote.py
+++ cogs/commands/moderation/vote.py
@@ -14,9 +14,9 @@ class Vote(commands.Cog):
             description = f'{text}',
             timestamp = ctx.message.created_at
         )
-        await ctx.send(embed = emb)
-        await ctx.message.add_reaction('<a:yes:754079238151340053>')
-        await self.bot.add_reaction('<a:no:754079450827718716> ')
+        message = await ctx.send(embed = emb)
+        await message.add_reaction('<a:yes:754079238151340053>')
+        await message.add_reaction('<a:no:754079450827718716> ')
 
 def setup(bot):
     bot.add_cog(Vote(bot))
