21c999460b013e850ca867194429e40ce332e50a
xzartsust <igru1234567@gmail.com>
2020-09-22 18:48:17 +0300

    update

--- cogs/test.py
+++ cogs/test.py
@@ -1,4 +1,5 @@
-   @commands.command(name='pause', aliases = ['pa', 'pau'])
+'''
+   @commands.command(name="pause", aliases = ["pa", "pau"])
     @commands.has_permissions()
     async def _pause(self, ctx: commands.Context):
         """Приостанавливает воспроизводимую в данный момент песню."""
@@ -7,7 +8,7 @@
             ctx.voice_state.voice.pause()
             await ctx.message.add_reaction("⏯")
 
-    @commands.command(name='resume', aliases=['r', 'res'])
+    @commands.command(name="resume", aliases=["r", "res"])
     @commands.has_permissions()
     async def _resume(self, ctx: commands.Context):
         """Возобновляет приостановленную в данный момент песню."""
@@ -16,7 +17,7 @@
             ctx.voice_state.voice.resume()
             await ctx.message.add_reaction("⏯")
 
-    @commands.command(name='stop", aliases = ["s", "st"])
+    @commands.command(name="stop", aliases = ["s", "st"])
     @commands.has_permissions()
     async def _stop(self, ctx: commands.Context):
         """Останавливает воспроизведение песни и очищает очередь."""
@@ -26,3 +27,4 @@
         if not ctx.voice_state.is_playing:
             ctx.voice_state.voice.stop()
             await ctx.message.add_reaction("⏹")
+'''
