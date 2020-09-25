74919c156f09c8ad37e813ef4e75090135ad318a
xzartsust <igru1234567@gmail.com>
2020-09-25 13:34:07 +0300

    update

--- cogs/test.py
+++ cogs/test.py
@@ -13,10 +13,10 @@ class Test(commands.Cog):
         tts = pyttsx3.init()
         voices = tts.getProperty('voices')
         tts.setProperty('voice', 'ru')
-        tts.setProperty('rate', 150)    # Скорость в % (может быть > 100)
+        tts.setProperty('rate', 150) 
         tts.setProperty('volume', 1)
         for voice in voices:
-            if voice.name == 'Microsoft Irina Desktop - Russian':
+            if voice.name:
                 tts.setProperty('voice', voice.id)
         tts.say('Привет')
         tts.runAndWait()
