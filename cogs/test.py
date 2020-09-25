ea9b52fc6f0cffd7d75eaee0a5717226b39e9321
xzartsust <igru1234567@gmail.com>
2020-09-25 13:37:48 +0300

    u[date

--- cogs/test.py
+++ cogs/test.py
@@ -9,17 +9,7 @@ class Test(commands.Cog):
     @commands.command()
     @commands.is_owner()
     async def test(self, ctx):
-        
-        tts = pyttsx3.init()
-        voices = tts.getProperty('voices')
-        tts.setProperty('voice', 'ru')
-        tts.setProperty('rate', 150) 
-        tts.setProperty('volume', 1)
-        for voice in voices:
-            if voice.name:
-                tts.setProperty('voice', voice.id)
-        tts.say('Привет')
-        tts.runAndWait()
+        pass
 
 def setup(bot):
     bot.add_cog(Test(bot))
