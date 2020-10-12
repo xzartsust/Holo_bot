66b35994bae182a11bfdd1e50c6603198d1dd182
xzartsust <68950796+xzartsust@users.noreply.github.com>
2020-10-12 21:16:20 +0300

    Update send.py

--- cogs/commands/moderation/send.py
+++ cogs/commands/moderation/send.py
@@ -8,10 +8,18 @@ class SendMessage(commands.Cog):
     @commands.command()
     @commands.has_permissions(administrator = True)
     async def send(self, ctx, *, text: str):
+        try:
+            
+            message = await ctx.send(text)
+            message_id = message.id
+            await ctx.message.delete()
         
-        message = await ctx.send(text)
-        message_id = message.id
-        await ctx.message.delete()
+        except Exception as e:
+             print(f'[{ctx.message.created_at}] [{ctx.message.guild.name}] [{ctx.message.guild.owner}] - [{e}]')
+        
+    async def cog_command_error(self, ctx: commands.Context, error: commands.CommandError):
+        await ctx.send('Произошла ошибка: {}'.format(str(error)))
+        print(f'[{ctx.message.created_at}] [{ctx.message.guild.name}] [{ctx.message.guild.owner}] - [{error}]')
         
 def setup(bot):
     bot.add_cog(SendMessage(bot))
