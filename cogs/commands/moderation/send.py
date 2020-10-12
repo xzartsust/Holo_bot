92511e2cecfee364b7933f26b06c72e6823fd0a0
xzartsust <68950796+xzartsust@users.noreply.github.com>
2020-10-12 20:34:36 +0300

    Create send.py

---
+++ cogs/commands/moderation/send.py
@@ -0,0 +1,15 @@
+import discord
+from discrod.ext import commands
+
+class SendMessage(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        
+    @commands.command()
+    @commands.has_permissions(administrator = True)
+    async def send(self, ctx, *, text: str):
+        
+        await ctx.send(text)
+        
+def setup(bot):
+    bot.add_cog(SendMessage(bot))
