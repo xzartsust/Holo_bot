7c4742b2337810c2f2f0d364aea3dec5b7240449
xzartsust <68950796+xzartsust@users.noreply.github.com>
2020-10-15 08:46:36 +0300

    Update private_channel.py

--- cogs/commands/moderation/command/private_channel.py
+++ cogs/commands/moderation/command/private_channel.py
@@ -37,7 +37,9 @@ class PrivateChannel(commands.Cog):
         channel_category = c_c[0]
 
             #if voice_channel is not None and channel_category is not None:
-        if before.channel.id == 754072936541061213:
+        channel_1 = get(guild.voice_channels, id=str(before.channel.id))
+        print(channel_1)
+        if channel_1 == 754072936541061213:
             maincategori = get(member.guild.categories, id = 743780552024260670)
             channel2 = await member.guild.create_voice_channel(name = f'Приватный({member.display_name})', category = maincategori)
             await channel2.set_permissions(member, connect = True, mute_members = True, move_members = True, manage_channels = True)
