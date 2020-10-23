cc8683ff5195ecd17a55323fdb0579937b8f2165
xzartsust <igru1234567@gmail.com>
2020-10-23 21:31:36 +0300

    up

--- cogs/commands/info/prefixserver.py
+++ cogs/commands/info/prefixserver.py
@@ -10,15 +10,6 @@ password = os.environ.get('PASSWORD')
 host = os.environ.get('HOST')
 port = os.environ.get('PORT')
 
-conn = psycopg2.connect(
-    database = f"{database}", 
-    user = f"{user}", 
-    password = f"{password}", 
-    host = f"{host}", 
-    port = "5432"
-)
-
-cursor = conn.cursor()
  
 class PrefixServer(commands.Cog):
     def __init__(self, bot):
@@ -26,14 +17,30 @@ class PrefixServer(commands.Cog):
 
     @commands.command(aliases = ['ps','sp'])
     async def prefixserver(self, ctx):
-
-
         guild = ctx.message.guild
-
-        try:
         
+        try:
+            conn = psycopg2.connect(
+                database = f"{database}", 
+                user = f"{user}", 
+                password = f"{password}", 
+                host = f"{host}", 
+                port = "5432"
+            )
+            cursor = conn.cursor()
+
             cursor.execute(f'SELECT prefix_guild FROM public."myBD" WHERE guild_id = \'{guild.id}\';')
             prefix = cursor.fetchone()
+        
+        except (Exception, psycopg2.Error) as error:
+            print ("Error while connecting to PostgreSQL", error)
+        finally:
+            if(conn):
+                cursor.close()
+                conn.close()
+                print("PostgreSQL connection is closed")
+
+        try:
 
             await ctx.send(f'Server Prefix: \"**{prefix[0]}**\"')
         
